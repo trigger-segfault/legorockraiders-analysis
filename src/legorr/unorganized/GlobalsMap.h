bf44e2bec02229094ee4bf96da1a713e7d1bdec1
Robert Jordan <jrob800@gmail.com>
2021-07-16 11:36:38 -0400

    Add GlobalsMap.h
    * GlobalsMap is a *very rough* outline of the global variable spaces used by LegoRR.
    * This is not comprehensive. The information is not fully correct on usage. And most importantly, the higher in the file you are, the less accurate it gets (I was tired)

---
+++ src/legorr/unorganized/GlobalsMap.h
@@ -0,0 +1,502 @@
+
+
+///// RNCCONST:
+// <LegoRR.exe @004b3d08>
+// <LegoRR.exe @004b3db0>
+///// ENDOFSETUPMODULE:
+
+
+//// STARTOFUNDEFINEDDATA:
+
+/// ADVISORSTABLE:
+// <LegoRR.exe @004b3db0>
+
+/// EMPTYSTR:
+// <LegoRR.exe @004b9a14>
+
+/// GAMEPLAYFROMHEREONOUT:
+
+/// MESSAGETEXTS:
+// <LegoRR.exe @00504608>
+// <LegoRR.exe @0050466c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0050466c>
+// <LegoRR.exe @00504870>
+
+/// WEAPONSMODULE:
+// <LegoRR.exe @00504870>
+// <LegoRR.exe @00506800>
+/// END:
+
+/// MAINWINDOWCMDANDRENDER:
+// <LegoRR.exe @00506800>
+// <LegoRR.exe @00507090>
+/// END:
+
+
+// (unused)
+// <LegoRR.exe @00507090>
+// <LegoRR.exe @00507098>
+
+/// CFGPROPERTYRESERVEDPOOLANDMODULE:
+// <LegoRR.exe @00507098>
+// <LegoRR.exe @00507524>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00507524>
+// <LegoRR.exe @00507528>
+
+/// IMAGEFONTRESERVEDPOOL:
+// <LegoRR.exe @00507528>
+// <LegoRR.exe @005075b0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005075b0>
+// <LegoRR.exe @005075b8>
+
+/// DIRECTSOUNDMODULE:
+// <LegoRR.exe @005075b8>
+// <LegoRR.exe @00534904>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00534904>
+// <LegoRR.exe @00534908>
+
+/// IMAGEBMPRESERVEDPOOL:
+// <LegoRR.exe @00534908>
+// <LegoRR.exe @00534994>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00534994>
+// <LegoRR.exe @00534998>
+
+/// AVIMODULE:
+// <LegoRR.exe @00534998>
+// <LegoRR.exe @005349a0>
+/// END:
+
+/// FILESYSTEM:
+// <LegoRR.exe @005349a0>
+// <LegoRR.exe @005353bc>
+/// END:
+
+/// SHAREDDATARESOURCESUNKSTRUCTFOUREIGHT:
+// <LegoRR.exe @005353c4>
+/// TABLE:
+// <LegoRR.exe @005353c8>
+/// SHAREDTEXTURES:
+// <LegoRR.exe @00535558>
+/// SHAREDTEXTURESBIIIIIIGTABLE:
+// <LegoRR.exe @0053555c>
+/// ANIMFRAMES:
+// <LegoRR.exe @0054155c>
+/// DIRECTTHREEDRESERVEDPOOLSTRUCTTHREEFOUR:
+// <LegoRR.exe @00541564>
+// <LegoRR.exe @005415dc>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005415dc>
+// <LegoRR.exe @005417e8>
+
+/// DIRECTDRAWSURFACELOCK:
+// <LegoRR.exe @005417e8>
+// <LegoRR.exe @00541834>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00541834>
+// <LegoRR.exe @00541838>
+
+/// SHAREDOBJECTSHUGEMODULETABLES:
+// <LegoRR.exe @00541838>
+// <LegoRR.exe @00545864>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00545864>
+// <LegoRR.exe @0054586c>
+
+/// SOUNDENABLED:
+// <LegoRR.exe @0054586c>
+// <LegoRR.exe @00545870>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00545870>
+// <LegoRR.exe @00545a00>
+
+/// MCIMUSICPLAYBACK:
+// <LegoRR.exe @00545a00>
+// <LegoRR.exe @00545a1c>
+/// END:
+
+/// SHAREDFILEBUFFERS:
+// <LegoRR.exe @00545a20>
+// <LegoRR.exe @005498a0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005498a0>
+// <LegoRR.exe @005498a8>
+
+/// DTHREEDERRORFUNCTION:
+// <LegoRR.exe @005498a8>
+// <LegoRR.exe @0054a0c0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0054a0c0>
+// <LegoRR.exe @0054a0c8>
+
+/// MICROSOFTCRUNTIME:
+// <LegoRR.exe @0054a0c8>
+// <LegoRR.exe @0054a514>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0054a514>
+// <LegoRR.exe @0054a520>
+
+/// BIGUNKNOWNLEVELDATAMODULEWITHTABLES:
+// <LegoRR.exe @0054a520>
+// <LegoRR.exe @0054cf0c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0054cf0c>
+// <LegoRR.exe @0054cf20>
+
+/// TOOLTIPSMODULE:
+// <LegoRR.exe @0054cf20>
+// <LegoRR.exe @005530e4>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005530e4>
+// <LegoRR.exe @005530e8>
+
+/// LEVELBLOCKSUNK:
+// <LegoRR.exe @005530e8>
+// <LegoRR.exe @005530f0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005530f0>
+// <LegoRR.exe @00553100>
+
+/// SMOKEANIMEFRAMES:
+// <LegoRR.exe @00553100>
+// <LegoRR.exe @00553134>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00553134>
+// <LegoRR.exe @00553140>
+
+/// UNKSCROLLINFOPANEL:
+// <LegoRR.exe @00553140>
+// <LegoRR.exe @00553940>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00553940>
+// <LegoRR.exe @0055396c>
+
+#pragma region BEGINBIGNERPS
+///// BEGINBIGNERPS:
+// <LegoRR.exe @0055396c>
+// <LegoRR.exe @00553980>
+// <LegoRR.exe @0055399c>
+// (unused)
+// <LegoRR.exe @00555adc>
+// <LegoRR.exe @00555adc>
+
+/// NERPSDECFLOATONE:
+// <LegoRR.exe @00555adc>
+// <LegoRR.exe @00555ae0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00555ae0>
+// <LegoRR.exe @00555efc>
+
+/// NERPSINCFLOATONE:
+// <LegoRR.exe @00555efc>
+// <LegoRR.exe @00555f00>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00555f00>
+// <LegoRR.exe @00556118>
+
+/// NERPSPILOTCOUNTERSTWO:
+// <LegoRR.exe @00556118>
+// <LegoRR.exe @00556124>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556124>
+// <LegoRR.exe @00556324>
+
+/// NERPSINCCOUNTERS:
+// <LegoRR.exe @00556324>
+// <LegoRR.exe @00556334>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556334>
+// <LegoRR.exe @00556738>
+
+/// FLOATSTWO:
+// <LegoRR.exe @00556738>
+// <LegoRR.exe @00556740>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556740>
+// <LegoRR.exe @00556b28>
+///// ENDBIGNERPS:
+#pragma endregion
+
+/// REWARDRESOURCES:
+// <LegoRR.exe @00556b28>
+// (unused)
+// <LegoRR.exe @00556b30>
+// <LegoRR.exe @00556b3c>
+// (end)
+// <LegoRR.exe @00556bd0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556bd0>
+// <LegoRR.exe @00556be0>
+
+/// PTLEVENTS:
+// <LegoRR.exe @00556be0>
+// <LegoRR.exe @00556d24>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556d24>
+// <LegoRR.exe @00556d40>
+
+/// NERPSMODULEB:
+// <LegoRR.exe @00556d40>
+// (unused)
+// <LegoRR.exe @00556d74>
+// <LegoRR.exe @00556da0>
+// (end)
+// <LegoRR.exe @00556df4>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00556df4>
+// <LegoRR.exe @00556e00>
+
+/// LOADINGSCREENLOADERPROFILE:
+// <LegoRR.exe @00556e00>
+// <LegoRR.exe @00557094>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00557094>
+// <LegoRR.exe @005570a0>
+
+/// CAMERADATA:
+// <LegoRR.exe @005570a0>
+// <LegoRR.exe @005570b0>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005570b0>
+// <LegoRR.exe @005570c0>
+
+/// GAMEDATA:
+// <LegoRR.exe @005570c0>
+// <LegoRR.exe @00557fb8>
+/// END:
+
+/// GAMETWOSTATEVARS:
+// <LegoRR.exe @00557fb8>
+// <LegoRR.exe @00557fc0>
+/// END:
+
+/// FRONTEND:
+// <LegoRR.exe @00557fc0>
+// <LegoRR.exe @0055882c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0055882c>
+// <LegoRR.exe @00558834>
+
+/// TESTERCALLSTUFF:
+// <LegoRR.exe @00558834>
+// <LegoRR.exe @00558844>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00558844>
+// <LegoRR.exe @00558850>
+
+/// OBJECTBUBBLESUI:
+// <LegoRR.exe @00558850>
+// <LegoRR.exe @0055899c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0055899c>
+// <LegoRR.exe @005589e4>
+
+/// STRUCTEIGHTTABLES:
+// <LegoRR.exe @005589e4>
+// <LegoRR.exe @00558d5c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00558d5c>
+// <LegoRR.exe @00558d68>
+
+/// RNCCOMPRESSION:
+// <LegoRR.exe @00558d68>
+// <LegoRR.exe @00559094>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00559094>
+// <LegoRR.exe @005590a0>
+
+/// MODESELECTION:
+// <LegoRR.exe @005590a0>
+int g_DriverModes_COUNT;
+// <LegoRR.exe @005764e4>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005764e4>
+// <LegoRR.exe @00576500>
+
+
+/// WADFILES:
+// <LegoRR.exe @00576500>
+// <LegoRR.exe @00576668>
+/// END:
+
+// (unused)
+// <LegoRR.exe @00576668>
+// <LegoRR.exe @005766a0>
+
+
+/// SHAREDWADSANDFILESCAN:
+// <LegoRR.exe @005766a0>
+// <LegoRR.exe @005774f8>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005774f8>
+// <LegoRR.exe @00577500>
+
+/// MCIERRORTEXT:
+// <LegoRR.exe @00577500>
+char g_mciErrorText[200];
+// <LegoRR.exe @005775c8>
+/// END:
+
+// (unused)
+// <LegoRR.exe @005775c8>
+// <LegoRR.exe @005775e0>
+
+/// INPUTKEYSTABLE:
+// <LegoRR.exe @005775e0>
+char* g_KEYS_TABLE[256];
+// <LegoRR.exe @005779e0>
+/// END:
+
+/// SCANFILEPATHSTABLE:
+// <LegoRR.exe @005779e0>
+// <LegoRR.exe @0076b9e4>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076b9e4>
+// <LegoRR.exe @0076ba00>
+
+/// INPUTSTATEMODULEA:
+// <LegoRR.exe @0076ba00>
+// <LegoRR.exe @0076bb10>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076bb10>
+// <LegoRR.exe @0076bb24>
+
+/// INPUTMOUSEPOSITION:
+// <LegoRR.exe @0076bb24>
+// <LegoRR.exe @0076bb2c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076bb2c>
+// <LegoRR.exe @0076bb34>
+
+/// INPUTMOUSEDELTAANDBUTTONS:
+// <LegoRR.exe @0076bb34>
+// <LegoRR.exe @0076bb3c>
+// (unused)
+// <LegoRR.exe @0076bb44>
+// <LegoRR.exe @0076bb5c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076bb5c>
+// <LegoRR.exe @0076bb80>
+
+/// INPUTSTATEMODULEBANDDIRECTDRAW:
+// <LegoRR.exe @0076bb80>
+// <LegoRR.exe @0076bcc8>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076bcc8>
+// <LegoRR.exe @0076bce0>
+
+/// RESERVEDPOOLSTRUCTTWENTY:
+// <LegoRR.exe @0076bce0>
+// <LegoRR.exe @0076bd6c>
+/// END:
+
+// (unused)
+// <LegoRR.exe @0076bd6c>
+// <LegoRR.exe @0076bd80>
+
+/// RESERVEDPOOLRESOURCEDATABIGSTRUCT:
+// <LegoRR.exe @0076bd80>
+// <LegoRR.exe @0076dd98>
+// <LegoRR.exe @0076dd9c>
+/// END:
+
+// (unused/unknown)
+// <LegoRR.exe @0076dd9c>
+// <LegoRR.exe @0076eda0>
+
+/// UNKNOWNCRT:
+// <LegoRR.exe @0076eda0>
+// <LegoRR.exe @0076f000>
+/// END:
+
+//// .IDATA:
+// <LegoRR.exe @0076f000>
+// <LegoRR.exe @00770010>
+/// .END:
+
+//// .RSRC:
+// <LegoRR.exe @00771000>
+// <LegoRR.exe @00771c00>
+/// .EOF:
