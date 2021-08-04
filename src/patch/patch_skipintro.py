792f992075071183aaebeb855db3ffb8ae99cee6
Robert Jordan <jrob800@gmail.com>
2021-08-03 21:54:59 -0400

    Update Python ASM patches and add SkipIntro patch script
    * Update patch_sfxgroup.py to print traceback.
    * Add patch_skipintro.py in the same style as patch_sfxgroup.py

--- src/patch/patch_skipintro.py
+++ src/patch/patch_skipintro.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-"""Apply patch to LEGO Rock Raiders masterpiece executable to fix sound effect groups.
+"""Apply patch to LEGO Rock Raiders masterpiece executable to make all opening intros skippable.
 """
 
 __version__ = '1.0.0'
-__date__    = '2021-06-26'
+__date__    = '2021-07-24'
 __author__  = 'Robert Jordan'
 
-__all__ = ['patch_file_sfxgroup', 'patch_data_sfxgroup', 'apply_patch_sfxgroup']
+__all__ = ['patch_file_skipintro', 'patch_data_skipintro', 'apply_patch_skipintro']
 
 #######################################################################################
 
@@ -19,49 +19,32 @@ from typing import Optional
 
 #region ## CONSTANTS ##
 
-### CALL       lego::snd::SFX_Load (FUN_0047ab30)
-# search for call to this function which returns our sfxIndex
-#  (only ever called by this function)
-SEARCH_DAT  = bytes.fromhex('e8 c2 5a 01 00')
+###  NEG   EAX
+###  SBB   EAX,EAX
+###  NEG   EAX
+###  PUSH  EAX
+###1 PUSH  [004a4b5c] = "LegoAvi"
+###1 CALL  void Boot_loadAVIProperty(char*,BOOL)
+###2 PUSH  [004a4b24] = "LMILogo"
+###2 CALL  void Boot_loadSplashProperty(char*,BOOL,char*)
+# search for both calls to intro functions, where the skippably boolean is not constant
+SEARCH_DAT1 = bytes.fromhex('f7 d8 1b c0 f7 d8 50 68 5c 4b 4a 00 e8 22 56 ff ff')
+SEARCH_DAT2 = bytes.fromhex('f7 d8 1b c0 f7 d8 50 68 24 4b 4a 00 e8 82 54 ff ff')
 
-###-  JBE        LAB_004650c6
-###+  JBE        LAB_004650c8
-# modify jump on `numParts == 0`
-#  (offset + 0x2 after insert)
-REPL_OFF1   = -0x66
-ORIG_DAT1   = bytes.fromhex('0f 86 bd 00 00 00')
-REPL_DAT1   = bytes.fromhex('0f 86 bf 00 00 00')
-###-  MOV        dword ptr [EDI],EAX=>SFXProperty->sfxIndex   = ??
-###-  JZ         LAB_004650a2
-###+  MOV        EDX,EAX
-###+  JZ         LAB_004650a4
-# dont't write sfxIndex yet, instead save it to EDX register (which is not being used atm)
-# modify jump on `sfxIndex == -1`
-#  (offset + 0x2 after insert)
-REPL_OFF2   =  0xb
-ORIG_DAT2   = bytes.fromhex('89 07 74 2a')
-REPL_DAT2   = bytes.fromhex('8b d0 74 2c')
-###-  JNZ        LAB_00465017
-###+  JNZ        LAB_00465017
-# modify jump on `do {} while (numParts != 0)`
-#  (offset - 0x2 after insert)
-REPL_OFF3   = 0x55
-ORIG_DAT3   = bytes.fromhex('0f 85 53 ff ff ff')
-REPL_DAT3   = bytes.fromhex('0f 85 51 ff ff ff')
-###+  MOV        dword ptr [EDI],EDX=>SFXProperty->sfxIndex   = ??
-###   JMP        LAB_004650aa
-# insert move instruction for `sfxIndex = EDX`
-#  (original instruction register now assigns value of EDX, not EAX)
-INSERT_OFF4 = 0x37 # (before)
-INSERT_DAT4 = bytes.fromhex('89 17')
-###   RET
-###=  ??
-###=  ??
-###=  ??
-# end of function, we need to shift everything by size of our inserted data (0x2)
-#  (we have plenty of room with 0x90 INT3 padding bytes to overwrite)
-END_OFF5    = 0x6a # (after RET)
-ORIG_DAT5   = bytes.fromhex('90 90 90')
+###-  NEG   EAX
+###-  SBB   EAX,EAX
+###-  NEG   EAX
+###-  PUSH  EAX
+###+  NOP
+###+  NOP
+###+  NOP
+###+  NOP
+###+  NOP
+###+  PUSH  0x1
+# strip useless operations and push constant 0x1 (true)
+REPL_OFF   = 0x0
+ORIG_DAT   = bytes.fromhex('f7 d8 1b c0 f7 d8 50')
+REPL_DAT   = bytes.fromhex('90 90 90 90 90 6a 01')
 
 #endregion
 
@@ -78,34 +61,20 @@ def apply_patch(data:bytearray) -> None:
     
     # make sure we found our data,
     # and that there's only one appearance of the pattern we're searching for
-    offset = data.find(SEARCH_DAT)
-    assert(offset != -1 and data.count(SEARCH_DAT) == 1)
+    offset1 = data.find(SEARCH_DAT1)
+    assert(offset1 != -1 and data.count(SEARCH_DAT1) == 1)
+    offset2 = data.find(SEARCH_DAT2)
+    assert(offset2 != -1 and data.count(SEARCH_DAT2) == 1)
 
     # create shorthands for all offsets
-    off1 = offset + REPL_OFF1
-    off2 = offset + REPL_OFF2
-    off3 = offset + REPL_OFF3
-    off4 = offset + INSERT_OFF4
-    off5 = offset + END_OFF5
+    off1 = offset1 + REPL_OFF
+    off2 = offset2 + REPL_OFF
 
     # replace instructions
-    assert(data[off1 : off1+len(REPL_DAT1)] == ORIG_DAT1)
-    data[off1 : off1+len(REPL_DAT1)] = REPL_DAT1
-    assert(data[off2 : off2+len(REPL_DAT2)] == ORIG_DAT2)
-    data[off2 : off2+len(REPL_DAT2)] = REPL_DAT2
-    assert(data[off3 : off3+len(REPL_DAT3)] == ORIG_DAT3)
-    data[off3 : off3+len(REPL_DAT3)] = REPL_DAT3
-
-    # ensure end of function
-    assert(data[off5 : off5+len(ORIG_DAT5)] == ORIG_DAT5)
-    # store a copy of data to shift (after insert)
-    shift_dat = data[off4 : off5]
-
-    # insert data (technically replace, so we don't offset the file contents)
-    data[off4 : off4+len(INSERT_DAT4)] = INSERT_DAT4
-
-    # finally store shifted data (offset by the size of inserted data)
-    data[off4+len(INSERT_DAT4) : off5+len(INSERT_DAT4)] = shift_dat
+    assert(data[off1 : off1+len(REPL_DAT)] == ORIG_DAT)
+    data[off1 : off1+len(REPL_DAT)] = REPL_DAT
+    assert(data[off2 : off2+len(REPL_DAT)] == ORIG_DAT)
+    data[off2 : off2+len(REPL_DAT)] = REPL_DAT
 
     # ensure the length of the file never changed
     assert(len(data) == orig_len)
@@ -143,9 +112,9 @@ def patch_file(src_file:str, dst_file:Optional[str]=None) -> None:
 #######################################################################################
 
 # assign import * names
-apply_patch_sfxgroup = apply_patch
-patch_data_sfxgroup = patch_data
-patch_file_sfxgroup = patch_file
+apply_patch_skipintro = apply_patch
+patch_data_skipintro = patch_data
+patch_file_skipintro = patch_file
 
 #######################################################################################
 
@@ -159,7 +128,7 @@ def main(argv:Optional[list]=None) -> int:
     #region ## PARSER SETUP ##
     import argparse
     parser = argparse.ArgumentParser(
-        description='Apply patch to LEGO Rock Raiders masterpiece executable to fix sound effect groups.',
+        description='Apply patch to LEGO Rock Raiders masterpiece executable make all opening intros skippable.',
         add_help=True)
     
     parser.add_argument('input', metavar='INPUTEXE', help='original executable to patch (LegoRR.exe)')
